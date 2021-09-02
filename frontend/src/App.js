e53a44725b05da561f5d0cad88b9b92db5da44a6
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-02 13:06:26 -0300

    creating title and columns w/ skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import Title from './components/Title';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Title/>
+      <div class="row">
+        <div class="one-half column">
+          <h3>Enter new movement</h3>
+        </div>
+
+        <div class="one-half column">
+          <h3>Resume:</h3>
+        </div>
+      </div>
+    </Fragment>
+    
   );
 }
 
---
+++ frontend/src/components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Title = () => {
+    return ( <h1>Weekly Budget Administrator</h1> );
+}
+ 
+export default Title;
