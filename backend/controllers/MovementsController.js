819ca1a6ba4b8a1ab803be04c1114e03c6772b1c
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-02 11:32:17 -0300

    put/edit and delete controllers added

--- backend/controllers/MovementsController.js
+++ backend/controllers/MovementsController.js
@@ -1,9 +1,6 @@
 import { Movement } from "../models/Movements.js";
 
 export const saveMovement = async (req, res, next) => {
-    
-    console.log(req.body);
-
     try {
         await Movement.create(req.body);
         res.json({message: 'Added successfully'});
@@ -23,3 +20,40 @@ export const getMovements = async (req, res, next) => {
     }
 }
 
+export const getMovement = async (req, res, next) => {
+    try {
+        const movement = await Movement.findByPk(req.params.id);
+        res.json(movement);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+export const editMovement = async (req, res, next) => {
+    try {
+        const {concept, amount} = req.body;
+        await Movement.update(
+            {concept,
+            amount},
+            {where: 
+                {id: req.params.id}});
+
+        const movement = await Movement.findByPk(req.params.id);
+        res.json(movement);
+
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+export const deleteMovement = async (req, res, next) => {
+    try {
+        await Movement.destroy({ where: {id : req.params.id}});
+        res.json({mensaje: 'Movement deleted successfully'});
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,25 @@
 import express from 'express';
 const router = express.Router();
-import {saveMovement, getMovements} from '../controllers/MovementsController.js';
+import {saveMovement, getMovements, getMovement, editMovement, deleteMovement} from '../controllers/MovementsController.js';
 
 
 router.get('/', (req, res) => {
     res.send('Budget');
 });
 
+// Add movement on DB
 router.post('/movements', saveMovement);
 
-router.get('/movements', 
-    getMovements
-);
+// Get movements from DB
+router.get('/movements', getMovements);
 
+// Get a single movement by id
+router.get('/movements/:id', getMovement);
+
+// Edit movement on DB
+router.put('/movements/:id', editMovement);
+
+// Delete movement on DB
+router.delete('/movements/:id', deleteMovement);
 
 export default router;
