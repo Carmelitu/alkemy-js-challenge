fae8bd1d19bf4246460941b3a78449a828a31b13
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-03 21:23:56 -0300

    axios and react router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,45 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import axiosClient from './config/axios';
+
+// Comps
 import Form from './components/Form';
 import Table from './components/Table';
 import Title from './components/Title';
 
 function App() {
   return (
-    <Fragment>
-      <div className="container">
-        <Title/>
-        <div class="row">
-          <div class="one-half column">
-            <h3>Enter new movement</h3>
-            <Form />
-          </div>
+    <Router>
+    <Switch>
+      <Route
+        exact path="/"
+        component={ () => <Fragment>
+          <Title/>
+          <Table />
+        </Fragment>}
+      />
+      
+      <Route
+        exact path="/new"
+        component={() => <Form />}
+      />
 
-          <div class="one-half column">
-            <h3>Summary</h3>
-            <Table />
-          </div>
-        </div>
-      </div>
-    </Fragment>
+      <Route
+        exact path="/movement/:id"
+
+        /*render={(props) => {
+          const cita = citas.filter(cita => cita._id === props.match.params.id)
+          console.log(cita);
+
+          return (
+          <Cita 
+            cita={cita[0]}
+            guardarConsulta={guardarConsulta}
+          />)
+        }}*/
+      />
+    </Switch>
+  </Router>
   );
 }
 
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -2,33 +2,40 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <form>
-            <div className="row">
-                <div className="u-full-width">
-                    <label for="concept-form">Concept</label>
-                    <input className="u-full-width" type="text" placeholder="Shopping" id="concept-form" />
-                </div>
+      <div className="container">
+        <div class="row">
+        <div class="u-full-width">
+            <h4>Enter new movement</h4>
+                <form>
+                    <div className="row">
+                        <div className="u-full-width">
+                            <label for="concept-form">Concept</label>
+                            <input className="u-full-width" type="text" placeholder="Shopping" id="concept-form" />
+                        </div>
+                    </div>
+                    <div className="u-full-width">
+                            <label for="amount-form">Date</label>
+                            <input className="u-full-width" type="date" id="amount-form"/>
+                        </div>
+                    <div className="row">
+                        <div className="one-half column">
+                            <label for="type-form">Type</label>
+                            <select class="u-full-width" id="type-form">
+                                <option value="">- Select an option -</option>
+                                <option value="income">Income</option>
+                                <option value="expenditure">Expenditure</option>
+                            </select>
+                        </div>
+                        <div className="one-half column">
+                            <label for="amount-form">Amount</label>
+                            <input className="u-full-width" type="number" placeholder="500" id="amount-form"/>
+                        </div>
+                    </div>
+                    <input className="button-primary u-full-width" type="submit" value="Submit"/>
+                </form>
             </div>
-            <div className="u-full-width">
-                    <label for="amount-form">Date</label>
-                    <input className="u-full-width" type="date" id="amount-form"/>
-                </div>
-            <div className="row">
-                <div className="one-half column">
-                    <label for="type-form">Type</label>
-                    <select class="u-full-width" id="type-form">
-                        <option value="">- Select an option -</option>
-                        <option value="income">Income</option>
-                        <option value="expenditure">Expenditure</option>
-                    </select>
-                </div>
-                <div className="one-half column">
-                    <label for="amount-form">Amount</label>
-                    <input className="u-full-width" type="number" placeholder="500" id="amount-form"/>
-                </div>
             </div>
-            <input className="button-primary u-full-width" type="submit" value="Submit"/>
-        </form>
+        </div>
     );
 }
  
